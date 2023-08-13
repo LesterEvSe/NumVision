7caa37df9073747f9efa6a23ef40152db26c0327
LesterEvSe <evgenii.nikolaevichfgh@gmail.com>
2023-08-13 17:24:43 +0300

    Implemented layers and fixed bug with sigmoid funciton

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import layer as lr
+import functions as func
 import numpy as np
 from PIL import Image
 
 image_matrix = np.array(Image.open('000000-num5.png').convert('L'))
 
 a0 = image_matrix.flatten()
-layer = lr.Layer(a0)
+layers = [lr.Layer(a0)]  # Layers, without output
+for i in range(1, 3):
+    layers.append(lr.Layer(layers[i-1].calculate_next_layer(func.sig)))
 
 
 # print(a0)
