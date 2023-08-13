2132279884411845e09291722df3cf63f9c8af48
В'ячеслав <suzium.trap@gmail.com>
2023-08-13 19:01:46 +0300

    Shrink to range [0;1]

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import layer as lr
 import functions as fn
 import neural_network as nn
 import numpy as np
+import cost as cs
 from PIL import Image
 
 image_matrix = np.array(Image.open('000000-num5.png').convert('L'))
 
-a = image_matrix.flatten()
+a = [a / 255 for a in image_matrix.flatten()]
 
 n = nn.NeuralNetwork([28*28, 16, 16, 10])
 
-print(n.calculate(a, fn.sig))
+print(cs.cost(n.calculate(a, fn.sig), [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]))
