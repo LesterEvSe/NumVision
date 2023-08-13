45418fff93f649100a1df248bd5487a6d8da7558
В'ячеслав <suzium.trap@gmail.com>
2023-08-13 16:49:13 +0300

    Add sigmoid and ReLU functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+import numpy as np
+from PIL import Image
+
+import matplotlib.pyplot as plt
+
+def sig(x):
+	return 1 / (1 + np.exp(-x))
+
+def dersig(x):
+	return sig(x) * (1 - sig(x))
+
+def ReLU(x):
+	return max(0, x)
+
+def derReLU(x):
+	return 1 if x > 0 else 0
