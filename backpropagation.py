Fixed bugs and implemented backpropagation func

backpropagation.py
@@ -4,14 +4,52 @@ import neural_network as nn
 
 class WeighStorage:
     def __init__(self, net: nn):
-        self.storage = [np.zeros((2,)) for _ in range(len(net.sizes))]
+        # We have Wnk and b(bias)k
+        # where n - current layer size, k - prev layer size
+        self.storage_W = [0] * len(net.layers)
+        self.storage_bias = [0] * len(net.layers)
 
+    def add(self, layer: int, weigh, bias):
+        self.storage_W[layer] = weigh
+        self.storage_bias[layer] = bias
 
-def calcGradient(net: nn):
-    # print(net.sizes)
-    # Last Layer recalculation
-    weigh = WeighStorage(net)
+    def getW(self, layer: int):
+        return self.storage_W[layer]
+    def getBias(self, layer: int):
+        return self.storage_bias[layer]
 
-    for i in range(len(net.sizes)-2, -1, -1):
-        pass
+    def printAll(self):
+        print("Biases")
+        for elem in self.storage_bias:
+            print(elem)
+
+        print("Weighs")
+        for matrix in self.storage_W:
+            print(matrix)
+
+def calcGradient(net: nn, real_ans):
+    weighs = WeighStorage(net)
+    derC0_prev = fn.derSquareDeviation(net.layers[-1].a, real_ans)
+
+    for ly in range(len(net.layers)-1, -1, -1):
+        W = np.zeros(net.layers[ly].getWShape())
+        bias = np.zeros(net.layers[ly].getBiasSize())
+        derC0_curr = np.zeros(W.shape[1])
+
+        prev_a = net.getLayerA(ly-1)
+        curr_z = net.layers[ly].z
+        W_old = net.layers[ly].W
+
+        # Calculate gradient. Hard formulas...
+        for j in range(W.shape[0]):
+            temp_foo = fn.derSig(curr_z[j])
+
+            for k in range(W.shape[1]):
+                W[j][k] = prev_a[k] * temp_foo * derC0_prev[j]
+                derC0_curr[k] += W_old[j][k]
+            bias[j] = temp_foo * derC0_prev[j]
+
+        weighs.add(ly, W, bias)
+        derC0_prev = derC0_curr
+    return weighs
 

functions.py
@@ -19,7 +19,7 @@ def squareDeviation(a_final, real_ans):
 	return sum([(a_final[i] - real_ans[i]) ** 2 for i in range(len(real_ans))])
 
 def derSquareDeviation(a_final, real_ans):
-	return sum([2*(a_final[i] - real_ans[i]) for i in range(len(real_ans))])
+	return [2*(a_final[i] - real_ans[i]) for i in range(len(real_ans))]
 
 def crossEntropy(a_final, real_ans):
 	def formula(a, y):
@@ -27,4 +27,4 @@ def crossEntropy(a_final, real_ans):
 	return sum(formula(a_final[i], real_ans[i]) for i in range(len(real_ans)))
 
 def derCrossEntropy(a_final, real_ans):
-	return sum([np.log(real_ans[i]) - np.log(1 - real_ans[i]) for i in range(len(real_ans))])
+	return [np.log(real_ans[i]) - np.log(1 - real_ans[i]) for i in range(len(real_ans))]

layer.py
@@ -1,23 +1,26 @@
 import numpy as np
 
 class Layer:
+    s_from = -1
+    s_to = 1
     def __init__(self, a_prev: np.array, layer_size: int, func):
-        self.W = np.random.rand(layer_size, a_prev.shape[0])
+        self.W = np.random.uniform(self.s_from, self.s_to,
+                                   size=(layer_size, a_prev.shape[0]))
         self.bias = np.zeros(layer_size)
-        self.a = self.calcLayer(a_prev, func)
+        self.z = self.calcLayer(a_prev)
+        self.a = func(self.z)
 
-    def calcLayer(self, a_prev, func):
-        return func(np.dot(self.W, a_prev) + self.bias)
+    def calcLayer(self, a_prev):
+        return np.dot(self.W, a_prev) + self.bias
 
-    def recalcLayer(self, W, bias):
-        self.W += W
+    def recalcLayer(self, weighs, bias):
+        self.W += weighs
         self.bias += bias
 
     def getNeurons(self):
         return self.a
 
-    def getWeight(self, i, j):
-        return self.W[i][j]
-    def getBias(self, i):
-        return self.bias[i]
-
+    def getWShape(self):
+        return self.W.shape
+    def getBiasSize(self):
+        return self.bias.shape[0]

main.py
@@ -14,4 +14,7 @@ net = nn.NeuralNetwork(inputImage)
 net.addLayer(16, fn.sig)
 net.addLayer(16, fn.sig)
 net.addLayer(10, fn.sig)
-# bp.calcGradient(net)
+num = np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0])
+
+weighs = bp.calcGradient(net, num)
+weighs.printAll()